6a88d0d8d545de3c1a204a22e1280c56df3dd8b9
Thijs <thijs@mediamonks.com>
2020-04-27 16:50:33 -0300

    Add unit test for testing compatibility with seng-event

--- test/DisposableEventListener.spec.ts
+++ test/DisposableEventListener.spec.ts
@@ -2,6 +2,7 @@ import {use, expect} from 'chai';
 import {spy} from 'sinon';
 import {DisposableEventListener} from '../src';
 import sinonChai from 'sinon-chai';
+import EventDispatcher, {AbstractEvent} from "seng-event";
 use(sinonChai);
 
 describe('DisposableEventListener', () => {
@@ -15,6 +16,30 @@ describe('DisposableEventListener', () => {
     div.dispatchEvent(new Event('type'));
     disposableEventListener.dispose();
     div.dispatchEvent(new Event('type'));
+
+    expect(handler).calledOnce;
+    expect(disposableEventListener.isDisposed()).to.equal(true)
+  });
+});
+
+
+describe('seng-event compatibility', () => {
+  it('should be possible to use the EventDispatcher from seng-event with DisposableEventListener', () => {
+
+    class BasicEvent extends AbstractEvent {
+      public clone(): BasicEvent {
+        return new BasicEvent(this.type, this.bubbles, this.cancelable, this.timeStamp !== 0);
+      }
+    }
+
+    const eventDispatcher = new EventDispatcher();
+    const handler = spy();
+
+    const disposableEventListener = new DisposableEventListener(eventDispatcher, 'type', handler);
+
+    eventDispatcher.dispatchEvent(new BasicEvent('type'));
+    disposableEventListener.dispose();
+    eventDispatcher.dispatchEvent(new BasicEvent('type'));
 
     expect(handler).calledOnce;
     expect(disposableEventListener.isDisposed()).to.equal(true)
